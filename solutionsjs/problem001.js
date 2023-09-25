e1a281c3a542f8880a22d621c46b3cef5f59605c
armorate <vickykr1905@gmail.com>
2023-09-26 05:07:23 +0530

    Multiples of 3 or 5

--- solutionsjs/problem001.js
+++ solutionsjs/problem001.js
@@ -1,11 +1,12 @@
 // problem001 - if we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
 function sum(limit) {
-    let accumulator = 0;
-    for (let i = 1; i < limit; i++) {
+    let i = 1, accumulator = 0;
+    while (i < limit) {
         if (i % 3 === 0 || i % 5 === 0) {
             accumulator += i;
         }
+        i++;
     }
     return accumulator;
 }
