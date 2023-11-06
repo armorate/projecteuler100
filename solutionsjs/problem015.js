c73a7643ca61f446b36dcdb4ec7e50549d98aed3
armorate <blinder404@gmail.com>
2023-11-06 17:19:57 +0530

    Lattice Paths

--- solutionsjs/problem015.js
+++ solutionsjs/problem015.js
@@ -1,4 +1,4 @@
-/*  Starting in the top left corner of a 2 * 2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+/*  problem015 - Starting in the top left corner of a 2 * 2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 image at https://projecteuler.net/resources/images/0015.png?1678992052
 How many such routes are there through a 20 * 20 grid?  */
 
